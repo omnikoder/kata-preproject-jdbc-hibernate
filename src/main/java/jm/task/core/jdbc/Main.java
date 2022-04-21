b84440cbc3ed238a63bb8bc365f4debb1ee764fb
omnikoder <98806400+omnikoder@users.noreply.github.com>
2022-04-21 23:09:06 +0500

    feat: утилита соединения с базой данных

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try (Connection databaseConnection = Util.connectToDatabase(Paths.get("connection.properties"))) {
+            System.out.println(databaseConnection.getCatalog());
+        } catch (SQLException | IOException ex) {
+            System.err.println("Не удалось установить соединение с базой данных.");
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,4 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
+    public static Connection connectToDatabase(Path propsFile) throws IOException, SQLException {
+        Properties connProps = new Properties();
+        try (InputStream input = Files.newInputStream(propsFile)) {
+            connProps.load(input);
+        }
+        return DriverManager.getConnection(
+                connProps.getProperty("url"),
+                connProps.getProperty("username"),
+                connProps.getProperty("password")
+        );
+    }
 }
