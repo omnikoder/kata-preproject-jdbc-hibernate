f89581f464408b3f5bf0787864e44798be891b5c
omnikoder <98806400+omnikoder@users.noreply.github.com>
2022-04-28 12:26:22 +0500

    refactor: убрано дублирование кода при использовании hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -6,168 +6,110 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class UserDaoHibernateImpl implements UserDao {
     private final SessionFactory sessionFactory = Util.getSessionFactory();
+    private Session session = null;
 
     public UserDaoHibernateImpl() {}
 
     @Override
     public void createUsersTable() {
-        Session session = null;
 
-        try {
-            session = this.sessionFactory.openSession();
-            session.beginTransaction();
-            session
-                    .createNativeQuery("create table if not exists users (" +
-                            "id bigint primary key auto_increment," +
-                            "name varchar(20) not null check (name like '__%')," +
-                            "lastname varchar(20) not null check (lastname != '')," +
-                            "age tinyint unsigned not null)",
-                            User.class)
-                    .executeUpdate();
-            session.getTransaction().commit();
-
-        } catch (HibernateException e) {
-            if (session != null) {
-                session.getTransaction().rollback();
-            }
-            throw new RuntimeException("Не удалось создать таблицу пользователей.\n" + e.getMessage());
-
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        beginTransaction("Не удалось создать таблицу пользователей.",
+                () -> Optional.of(this.session
+                        .createNativeQuery(
+                                "create table if not exists users (" +
+                                        "id bigint primary key auto_increment," +
+                                        "name varchar(20) not null check (name like '__%')," +
+                                        "lastname varchar(20) not null check (lastname != '')," +
+                                        "age tinyint unsigned not null)",
+                                User.class)
+                        .executeUpdate())
+        );
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = null;
 
-        try {
-            session = this.sessionFactory.openSession();
-            session.beginTransaction();
-            session
-                    .createNativeQuery("drop table if exists users", User.class)
-                    .executeUpdate();
-            session.getTransaction().commit();
-
-        } catch (HibernateException e) {
-            if (session != null) {
-                session.getTransaction().rollback();
-            }
-            throw new RuntimeException("Не удалось удалить таблицу пользователей.\n" + e.getMessage());
-
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        beginTransaction("Не удалось удалить таблицу пользователей.",
+                () -> Optional.of(this.session
+                        .createNativeQuery("drop table if exists users", User.class)
+                        .executeUpdate()
+                )
+        );
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = null;
 
-        try {
-            session = this.sessionFactory.openSession();
-            session.beginTransaction();
-            session.persist(new User(name, lastName, age));
-            session.getTransaction().commit();
-
-        } catch (HibernateException e) {
-            if (session != null) {
-                session.getTransaction().rollback();
-            }
-            throw new RuntimeException(String.format("Не удалось сохранить пользователя - [%s, %s, %d]\n%s",
-                    name, lastName, age, e.getMessage()));
-
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        beginTransaction(String.format("Не удалось сохранить пользователя - [%s, %s, %d].", name, lastName, age),
+                () -> {
+                    this.session.persist(new User(name, lastName, age));
+                    return Optional.empty();
+                }
+        );
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = null;
 
-        try {
-            session = this.sessionFactory.openSession();
-            session.beginTransaction();
-            User user = session.get(User.class, id);
-            if (user != null) {
-                session.remove(user);
-            }
-            session.getTransaction().commit();
-
-        } catch (HibernateException e) {
-            if (session != null) {
-                session.getTransaction().rollback();
-            }
-            throw new RuntimeException("Не удалось удалить пользователя с id: " + id + '\n' + e.getMessage());
-
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        beginTransaction("Не удалось удалить пользователя с id: " + id, () -> {
+            Optional.ofNullable(
+                    this.session.get(User.class, id)).ifPresent((user) -> this.session.remove(user)
+            );
+            return Optional.empty();
+        });
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = null;
-        List<User> users = null;
 
-        try {
-            session = this.sessionFactory.openSession();
-            session.beginTransaction();
-            users = session
-                    .createQuery("from User", User.class)
-                    .list();
-            session.getTransaction().commit();
-
-        } catch (HibernateException e) {
-            if (session != null) {
-                session.getTransaction().rollback();
-            }
-            throw new RuntimeException("Не удалось получить ползователей.\n" + e.getMessage());
-
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-
-        return users;
+        return beginTransaction("Не удалось получить ползователей.",
+                () -> Optional.of(this.session
+                        .createQuery("from User", User.class)
+                        .list()
+                )).orElse(new ArrayList<>());
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = null;
+
+        beginTransaction("Не удалось очистить таблицу пользователей.",
+                () -> Optional.of(this.session
+                        .createNativeQuery("truncate table users", User.class)
+                        .executeUpdate()
+                )
+        );
+    }
+
+    private <T> Optional<T> beginTransaction(String errorMessage, Supplier<Optional<T>> transaction) {
+        Optional<T> result;
 
         try {
-            session = this.sessionFactory.openSession();
-            session.beginTransaction();
-            session
-                    .createNativeQuery("truncate table users", User.class)
-                    .executeUpdate();
-            session.getTransaction().commit();
+            this.session = this.sessionFactory.openSession();
+            this.session.beginTransaction();
+
+            result = transaction.get();
+
+            this.session.getTransaction().commit();
 
         } catch (HibernateException e) {
-            if (session != null) {
-                session.getTransaction().rollback();
+            if (this.session != null) {
+                this.session.getTransaction().rollback();
             }
-            throw new RuntimeException("Не удалось очистить таблицу пользователей.\n" + e.getMessage());
+            throw new RuntimeException(errorMessage + '\n' + e.getMessage());
 
         } finally {
-            if (session != null) {
-                session.close();
+            if (this.session != null) {
+                this.session.close();
             }
         }
+
+        return result;
     }
 }
